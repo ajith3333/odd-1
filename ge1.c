33615596164d2e63a3538de96d2089bc64091081
GEETHAprakash <35689211+GEETHAprakash@users.noreply.github.com>
2018-01-31 14:11:24 +0530

    ge1.c

---
+++ ge1.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a;
+clrscr ();
+printf("enter the number";
+scanf("%d",&a);
+if(a<0)
+printf("enter the number is positive");
+else
+if(a>0)
+printf("enter the number is negative);
+else
+printf("enter the number is negative or postive");
+getch ();
+}
