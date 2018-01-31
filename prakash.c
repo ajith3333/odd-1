2a7fabf2a7dd4199a87f33b65fb404e1460542a6
GEETHAprakash <35689211+GEETHAprakash@users.noreply.github.com>
2018-01-31 14:25:50 +0530

    prakash.c

---
+++ prakash.c
@@ -0,0 +1,14 @@
+#include<stdio.h>
+#include<conio.h>
+void main ()
+{
+int a;
+printf( "enter the value a");
+scanf("%d",&a);
+//True if a is divisible by 2
+if(a%2==o)
+printf("%d is even",a);
+else
+printf("%d is odd",a);
+getch ();
+}
