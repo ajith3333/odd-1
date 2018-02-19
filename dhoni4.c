2a1486c5c5c5bfbd3cbd8aef77fe4cff6dc068cf
GEETHAprakash <35689211+GEETHAprakash@users.noreply.github.com>
2018-02-19 13:33:46 +0530

    dhoni4.c

---
+++ dhoni4.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>  
+ #include<conio.h>  
+void main()
+{  
+    int counter; 
+    printf("range numbers between 1 to 10\n");  
+    /* 
+     * Initialize counter with 1, and increment it in every iteration.
+     * For every value of counter check whether it is even number or
+     * not and print it accordingly 
+     
+       
+      
+   
+    return 0;  
+}
